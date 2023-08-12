806e8729767b1891cecac93ed834d4c36e87477e
yuvarajcdckap <yuvarajcdckap@gmail.com>
2023-08-12 13:54:13 +0530

    task detailing page UI work

--- JS/listing.js
+++ JS/listing.js
@@ -391,6 +391,12 @@ $(document).ready(function () {
   }
 
 });
+// -----------------------------------------
+let popUpclose = document.querySelector('#popUpCloseBtn');
+popUpclose.addEventListener("click", () => {
+  popUpWnd.classList.toggle('invisible');
+});
+// -----------------------------
 
 let taskDiv = document.querySelector('.taskListDiv')
 
@@ -428,7 +434,10 @@ function datas(data) {
           <div class="text-base leading-6 text-gray-900 no-underline " id="modal-title">
             <div class="change">
               <div class="Task-progress pt-px	">
-                <input type="checkbox" class="taskCheckBox" id="checkBox" />
+                  <div class="round">
+                    <input type="checkbox" id="checkbox" class="taskCheckBox"/>
+                    <label for="checkbox"></label>
+                  </div>
               </div>
               <div class="make-changes">
                 <button><i class="fa-solid fa-pen"></i></button>
@@ -446,38 +455,16 @@ function datas(data) {
     }).join("")
 
     taskDiv.innerHTML = datas
+
   }
   else {
     let emptyMsg = `
     <div>
     <p>Please Add Task</p>
-    </div>
-    `.join("")
+    </div>`
     taskDiv.innerHTML = emptyMsg;
   }
 
-
-  // ======================================
-  closeWindow()
-  // ==============================================================
-
-  let commentBtn = document.querySelectorAll(".add-comment-btn");
-  let commentInput = document.querySelectorAll(".model-title ");
-  let TaskCompleted = document.querySelectorAll("#checkBox");
-  let task_name = document.querySelectorAll(".task-inner-div");
-
-  for (let j = 0; j < commentBtn.length; j++) {
-
-    commentBtn[j].addEventListener("click", () => {
-      commentInput[j].classList.toggle("addvisibility")
-    })
-
-    TaskCompleted[j].addEventListener("click", () => {
-      task_name[j].classList.toggle("completedTask")
-
-
-    })
-  }
 
   // ======================================
 
@@ -490,18 +477,43 @@ function datas(data) {
       $(tasks_list[i]).hover(function () {
         $(btnDiv[i]).addClass('show')
       }, function () {
-        $(btnDiv[i]).removeClass('show'),
-          $(commentInput[i]).removeClass('addvisibility')
+        $(btnDiv[i]).removeClass('show')
       });
     }
   });
 
-}
+
+  // ==============================================================
+
+  let commentBtn = document.querySelectorAll(".add-comment-btn");
+  let commentInput = document.querySelectorAll(".model-title ");
+  let TaskCompleted = document.querySelectorAll("#checkbox");
+  let task_name = document.querySelectorAll(".task-inner-div");
+
+  for (let j = 0; j < commentBtn.length; j++) {
+
+    commentBtn[j].addEventListener("click", () => {
+      commentInput[j].classList.toggle("addvisibility")
+    })
+
+    TaskCompleted[j].addEventListener("click", () => {
+      task_name[j].classList.toggle("completedTask")
+      setTimeout(() => {
+        tasks_list[j].remove()
+      }, 250);
+    })
+  }
 
 
-function closeWindow(params) {
-  let popUpclose = document.querySelector('#popUpCloseBtn');
-  popUpclose.addEventListener("click", () => {
-    popUpWnd.classList.toggle('invisible');
-  });
 }
+
+  // ======================================
+
+  // let viewDiv1 = document.querySelector('.viewBtnDiv1');
+  // let viewDiv2 = document.querySelector('.viewBtnDiv2');
+  // let viewDiv3 = document.querySelector('.viewBtnDiv3');
+  // let viewDiv4 = document.querySelector('.viewBtnDiv4');
+  // console.log(viewDiv1.parentElement)
+  
+  // viewDiv1.innerHTML = `<button class="showMoreBtn" id="getid" data-id="<?php echo "1" ?>" name="matrixId">View Task..</button>`
+
