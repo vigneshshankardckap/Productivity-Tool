06d69e4e24296bd0bf2753244340a312892e1424
prasanthndckap <prasanthndckap@gmail.com>
2023-08-29 17:12:41 +0530

    profile photo updated

--- JS/listing.js
+++ JS/listing.js
@@ -1160,8 +1160,22 @@ function profilePage(datas) {
 editProfilebtn = document.querySelector('.editProfile');
 
 profileinputs = document.querySelector('.Profile-data');
-
-
+// -----------
+function readURL(input) {
+  if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+          $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+          $('#imagePreview').hide();
+          $('#imagePreview').fadeIn(650);
+      }
+      reader.readAsDataURL(input.files[0]);
+  }
+}
+$("#imageUpload").change(function() {
+  readURL(this);
+}); 
+// ------- 
 
 // =================================================================================
 
