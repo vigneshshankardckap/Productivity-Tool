03bd6deeb4df3d1d7e8ca5fbb4ccbe30e89a5f7e
vigneshshankardckap <vigneshshankardckap@gmail.com>
2023-08-30 15:40:00 +0530

    phase one completed

--- signup.php
+++ signup.php
@@ -89,9 +89,9 @@ if(isset($_GET["code"])){
                     <!-- <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a> -->
                 </div>
                 <span>or use your account</span>
-                <input type="email" placeholder="Email" value="sunil123@gmail.com" required name="email" pattern="[a-z,A-Z,0-9,@,.]*"/>
+                <input type="email" placeholder="Email" value="" required name="email" pattern="[a-z,A-Z,0-9,@,.]*"/>
                 <p class="emailError">Please Fill Up Your Email </p>
-                <input type="password" value="sunil123" placeholder="Password" required name="password"/>
+                <input type="password" value="" placeholder="Password" required name="password"/>
                 <p class="passError">Please Fill Up Your Password </p>
                 <button>login</button>
             </form>
